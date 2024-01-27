c38454eef1a372b565156c733824131527a28935
Toi Yoshimaru <0maru.dev@gmail.com>
2024-01-28 01:14:56 +0900

    ステータスバーからアプリを起動できるようにした

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -3,7 +3,27 @@ import FlutterMacOS
 
 @NSApplicationMain
 class AppDelegate: FlutterAppDelegate {
+  var statusBar: StatusBarController?
+  var popover = NSPopover.init()
+  
+  override init() {
+    popover.behavior = NSPopover.Behavior.transient
+  }
+  
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-    return true
+    return false
+  }
+  
+  override func applicationDidFinishLaunching(_ notification: Notification) {
+    let controller: FlutterViewController = mainFlutterWindow?.contentViewController as! FlutterViewController
+    popover.contentSize = NSSize(width: 300, height: 300)
+    popover.contentViewController = controller
+    statusBar = StatusBarController.init(popover)
+    guard let window = mainFlutterWindow else {
+      print("mainFlutterWindow is nil")
+      return
+    }
+    window.close()
+    super.applicationDidFinishLaunching(notification)
   }
 }
--- macos/Runner/StatusBarController.swift
+++ macos/Runner/StatusBarController.swift
@@ -2,7 +2,41 @@
 //  StatusBarController.swift
 //  Runner
 //
-//  Created by maru on 2024/01/27.
+//  Created by maru on 2024/01/28.
 //
 
-import Foundation
+import AppKit
+
+class StatusBarController {
+  private var statusBar: NSStatusBar
+  private var statusItem: NSStatusItem
+  private var popover: NSPopover
+  
+  init(_ popover: NSPopover) {
+    self.popover = popover
+    statusBar = NSStatusBar.init()
+    statusItem = statusBar.statusItem(withLength: 28.0)
+    
+    if let statusBarButton = statusItem.button {
+      statusBarButton.image = #imageLiteral(resourceName: "StatusbarICon")
+      statusBarButton.image?.size = NSSize(width: 18, height: 18)
+      statusBarButton.image?.isTemplate = true
+      statusBarButton.action = #selector(togglePopover(sender:))
+      statusBarButton.target = self
+    }
+  }
+  
+  @objc func togglePopover(sender: AnyObject) {
+    if (popover.isShown) {
+      popover.performClose(sender)
+    } else {
+      if let statusBarButton  = statusItem.button {
+        popover.show(
+          relativeTo: statusBarButton.bounds,
+          of: statusBarButton,
+          preferredEdge: NSRectEdge.minX
+        )
+      }
+    }
+  }
+}
