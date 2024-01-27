21800fc3353f5e1407820588bc5d017e42777db7
Toi Yoshimaru <0maru.dev@gmail.com>
2024-01-28 00:03:01 +0900

    ios, android, web は削除

--- android/app/src/main/kotlin/com/example/flutter_desktop_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flutter_desktop_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ macos/Runner/StatusBarController.swift
@@ -0,0 +1,8 @@
+//
+//  StatusBarController.swift
+//  Runner
+//
+//  Created by maru on 2024/01/27.
+//
+
+import Foundation
